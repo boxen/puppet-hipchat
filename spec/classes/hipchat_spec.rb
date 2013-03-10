eb8dbab20c29112dba7e6e981248f2bc6f5295ce
Adam Ochonicki <adam@fromonesrc.com>
2013-03-10 13:24:32 -0400

    fix failing specs

--- spec/classes/hipchat_spec.rb
+++ spec/classes/hipchat_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe 'hipchat' do
+
   it { should contain_class('hipchat') }
   it { should contain_package('HipChat').with_provider('compressed_app') }
   it { should contain_package('HipChat').with_source('http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.22-01uennzomg01wmg.zip') }
+
 end
