e2c8af491c3231b93164b4d633e347b4fabf31dc
Adam Ochonicki <adam@fromonesrc.com>
2013-04-02 20:21:40 -0400

    Bump version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class hipchat {
   package { 'HipChat':
     provider   => 'compressed_app',
-    source => 'http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.23-01uennzomg01wmg.zip'
+    source => 'http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.29-01uennzomg01wmg.zip'
   }
 }
