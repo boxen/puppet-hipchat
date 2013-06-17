652b2908ab93900e62f66ec451f9e6b90e934c7e
Adam Ochonicki <adam@fromonesrc.com>
2013-06-17 14:05:31 -0400

    Bump version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class hipchat {
   package { 'HipChat':
     provider   => 'compressed_app',
-    source => 'http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.32-01uennzomg01wmg.zip'
+    source => 'http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.35-01uennzomg01wmg.zip'
   }
 }
--- spec/classes/hipchat_spec.rb
+++ spec/classes/hipchat_spec.rb
@@ -4,6 +4,6 @@ describe 'hipchat' do
 
   it { should contain_class('hipchat') }
   it { should contain_package('HipChat').with_provider('compressed_app') }
-  it { should contain_package('HipChat').with_source('http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.32-01uennzomg01wmg.zip') }
+  it { should contain_package('HipChat').with_source('http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.35-01uennzomg01wmg.zip') }
 
 end
