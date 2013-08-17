145eb79868ffb39dd84e05b469ac3a1b3665ff07
Ben Jackson <ben@adaptivelab.com>
2013-08-17 13:29:45 +0100

    Updated to HipChat 2.3

--- spec/classes/hipchat_spec.rb
+++ spec/classes/hipchat_spec.rb
@@ -5,7 +5,7 @@ describe 'hipchat' do
     should contain_class('hipchat')
     should contain_package('HipChat').with({
       :provider => 'compressed_app',
-      :source   => 'http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.0.zip'
+      :source   => 'http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.3.zip'
     })
   end
 end
