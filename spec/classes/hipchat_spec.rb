674bb6f033c46ed6246bbbfdcf0c141fc437df61
Nathaniel Waisbrot <github.com@waisbrot.net>
2014-06-08 01:14:31 -0400

    Use the 'latest' download link
    Use 'latest' link, which redirects to the real file.
    Specify the flavor, since there's no file extension to guess from.

--- spec/classes/hipchat_spec.rb
+++ spec/classes/hipchat_spec.rb
@@ -5,7 +5,8 @@ describe 'hipchat' do
     should contain_class('hipchat')
     should contain_package('HipChat').with({
       :provider => 'compressed_app',
-      :source   => 'http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.5.4-83.zip'
+      :source   => 'https://www.hipchat.com/downloads/latest/mac',
+      :flavor   => 'zip',
     })
   end
 end
